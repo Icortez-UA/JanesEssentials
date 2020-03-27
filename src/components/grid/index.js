a8da008c55255c76f26bba8b9ac0f566a479d28d
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-03-27 14:17:32 -0700

    created our grid  components

---
+++ src/components/grid/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export function Container({fluid,children}){
+    return <div className= {`container`}>{children}</div>;
+}
+
+export function Row({children}){
+    return <div className="row">{children}</div>
+}
+
+export function Col({size,children}){
+    return (
+        <div
+        className={"col " + size}
+    >
+    {children}
+    </div>
+    )
+}
