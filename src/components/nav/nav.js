d17aa372cb8981e1c7739806e0284882b8e3db67
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-03-27 14:24:29 -0700

    update

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,38 +1,52 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles"
+const useStyles = makeStyles((theme) => ({
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  }
+
+})
+);
 
 function NavTabs() {
-  // We'll go into the Hooks API later, for now, we are just using some code
-  // from the react-router docs (https://reacttraining.com/react-router/web/api/Hooks/uselocation)
-  // This allows the component to check the route any time the user uses a link to navigate.
-  const location = useLocation();
+
+  const classes = useStyles();
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
+    <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+    <Toolbar>
+      <Typography variant="h6" color="inherit" noWrap className= {classes.toolbarTitle} >
+        Jane's Essentials
+      </Typography>
+      <nav>
         <Link
-          to="/about"
-          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
+          to="/home"
+          className={classes.link}
         >
-          About
+          Home
         </Link>
-      </li>
-      <li className="nav-item">
         <Link
-          to="/discover"
-          className={location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+          to="/all"
+          className={classes.link}
         >
-          discover
+          All
         </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/search"
-          className={location.pathname === "/search" ? "nav-link active" : "nav-link"}
-        >
-         search
-        </Link>
-      </li>
-    </ul>
+      </nav>
+      <Button href="#" color="primary" variant="outlined">
+        Login
+      </Button>
+    </Toolbar>
+  </AppBar>
   );
 }
 
