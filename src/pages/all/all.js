6a2cd236ecc4542dcd91ce40cccc4da8ea2e3209
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-03-31 21:07:27 -0700

    update home and all page

--- src/pages/all/all.js
+++ src/pages/all/all.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import Panel from "../../components/cardpanel/Panel";
+import {Col, Container, Row} from "../../components/grid/index"
+import Card from "../../components/Card/card"
+import All from "../../utils/all-strains.json"
 
 function AllStrains(){
 
-    return <div>
-        <CssBaseline />
-        <Container maxWidth="sm" component="main">
-        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-          Janes Essentials
-        </Typography>
-        <Typography variant="subtitle2" align="center" color="textSecondary" component="p">
-        This web application will allow users to search through cannabis strains and have quick access to key information to help them make an informed choice on what type of marijuana they would like to purchase beforehand.Users will be able to search through all of the 2000+ strains and decide which is best for their own needs. Our strain pages consist of a description of said strain, its positive and negative affects. Also, each specific page will contain an identity number and what each strain should be used for medically. Thanks for using Jane's Essentials!
-        </Typography>
-      </Container>
-        <Container maxWidth="md" component="main">
-            <Card>
-            <CardHeader
-                  title= "test"
-                />
+  return <div>
+  <Panel title="Welcome to Janes Essentials"></Panel>
+  <Row>
+    <Container>
+      <Col size="s6 m3">
+          <Card classes="card">
+            <Card classes="card-content">
+
             </Card>
-        </Container>
-        </div>
+          </Card>
+      </Col>
+      <Col size="s6 m3">
+        <Card classes= "card">
+          <Card classes="card-content">
+
+          </Card>
+        </Card>
+      </Col>
+
+    </Container>
+  </Row>
+  </div>
 
 }
 export default AllStrains;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Panel from "../../components/cardpanel/Panel";
 import {Col, Container, Row} from "../../components/grid/index"
+import Card from "../../components/Card/card"
 
 function Home(){
     return <div>
       <Panel title="Welcome to Janes Essentials"></Panel>
+      <Row>
+        <Container>
+          <Col size="s6 md3">
+              <Card classes="card">
+                <Card classes="card-content">
+
+                </Card>
+              </Card>
+          </Col>
+
+        </Container>
+      </Row>
       </div>
 
 }
