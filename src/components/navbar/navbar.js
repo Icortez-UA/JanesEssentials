310859513a85046d2828011c2f16e74d8cb893b4
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-04-01 00:53:08 -0700

    added logo to navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { Link} from "react-router-dom";
 import Side from "../sidenav/sidenav";
+import Logo from "../../assets/imgs/janelogo.png"
 
 function navbar(){
+
+  const styles ={
+    width: 70, 
+    marginleft:  20,
+    margintop: -55,
+    Height: 65
+
+  }
+
+
+
    return <div>
    <nav className="grey">
       <div className="nav-wrapper">
+      <Link to="/home" className="brand-logo">
+        <img className="responsive-image" src={Logo} id="navlogo"  style={styles}></img></Link>
       <a data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-   <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
        <li><Link
           to="/home"
         
