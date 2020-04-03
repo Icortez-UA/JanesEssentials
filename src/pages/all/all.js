3867bb028536cafe2bddd1a61fc36644de3500fd
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-04-03 13:30:57 -0700

    Update all.js
    to include sort by strain

--- src/pages/all/all.js
+++ src/pages/all/all.js
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react';
-import M from "materialize-css"
+import React, {useState,useEffect} from 'react';
+import M  from "materialize-css"
 import Panel from "../../components/cardpanel/Panel";
 import {Col, Container, Row} from "../../components/grid/index"
 import Card from "../../components/Card/card"
-import All from "../../utils/all-strains.json"
+import All from "../../utils/data1.json"
+import OG from "../../utils/janesSampledata.json"
 import Logo from "../../assets/imgs/icon-192x192.png"
 import Banner from "../../components/Parallax/parallax"
 import BannerImg from "../../assets/imgs/janesParalax.jpg"
@@ -11,10 +12,32 @@ import BannerImg from "../../assets/imgs/janesParalax.jpg"
 
 
 function AllStrains(){
+  const [strain,setStrain] = useState(All.data);
+  const [ogStrain,setOgstrain] = useState(OG);
+  const [dropdown,setDropdown] = useState("");
+
+  const filterResults = (choice,obj) =>{
+    if(choice === "DEFAULT"){
+      return obj;
+    }
+    else if (choice === "All"){
+      return obj;
+    }
+    else{
+    var result = obj.filter((a) =>{
+      return a.Value_race == choice
+  });
+      return result;
+    }
+
+  }
   useEffect(()=>{
+    //setStrain(strain.sort((a, b) => (a.seedCompany.name > b.seedCompany.name) ? 1 : -1))
+    
     M.AutoInit();
+
   
-  })
+  },[])
 
   return <div>
   <Banner classes="parallax-container">
@@ -23,16 +46,29 @@ function AllStrains(){
     </Banner>
   </Banner>
   
+  
+ <Row>
+  <div className="input-field col s4 offset-s4">
+    <select defaultValue={'DEFAULT'} onChange={e=> setOgstrain(filterResults(e.currentTarget.value,OG)) }>
+      <option value="DEFAULT" disabled >Choose your Strain!</option>
+      <option value="All">All</option>
+      <option value="hybrid">Hybrid</option>
+      <option value="indica">Indica</option>
+      <option value="sativa">Sativa</option>
+    </select>
+  </div>
+ </Row>
+
   <Row>
     <Container>
-      {All.slice(0,52).map(All=>(
-        <Col size="s6 m4">
-          <Card classes="card hoverable medium">
-          <Card classes="card-image waves-effect waves-block waves-light">
-          <img data-id='+res[i].id+' src={Logo} alt="logo"></img></Card>
+      {ogStrain.slice(0,150).map((ogStrain,index)=>(
+        <Col key={index} size="s6 m4">
+          <Card  classes="card hoverable medium">
+          <Card  classes="card-image waves-effect waves-block waves-light">
+          <img  src={Logo} alt="logo"></img></Card>
             <Card classes="card-content">
               <Card classes="card-title grey-text text-darken-4">
-                {All.Name}
+                {ogStrain.Name}
               </Card>
             </Card>
           </Card>
