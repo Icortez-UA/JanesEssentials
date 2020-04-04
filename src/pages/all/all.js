8c0f46214be73cf1508777e45bed86dc2d386c91
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-04-04 01:27:11 -0700

    update all
    added search function
    added icon cdn

--- src/pages/all/all.js
+++ src/pages/all/all.js
@@ -1,9 +1,9 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState,useEffect,useRef} from 'react';
 import M  from "materialize-css"
-import Panel from "../../components/cardpanel/Panel";
+//import Panel from "../../components/cardpanel/Panel";
 import {Col, Container, Row} from "../../components/grid/index"
 import Card from "../../components/Card/card"
-import All from "../../utils/data1.json"
+//import All from "../../utils/data1.json"
 import OG from "../../utils/janesSampledata.json"
 import Logo from "../../assets/imgs/icon-192x192.png"
 import Banner from "../../components/Parallax/parallax"
@@ -12,9 +12,16 @@ import BannerImg from "../../assets/imgs/janesParalax.jpg"
 
 
 function AllStrains(){
-  const [strain,setStrain] = useState(All.data);
+  //const [strain,setStrain] = useState(All.data);
   const [ogStrain,setOgstrain] = useState(OG);
+  const [searchTerm, setSearchTerm] = useState("");
 
+
+ 
+
+  const handleChange = (event) => {
+     setSearchTerm(event.target.value);
+   };
 
   const filterResults = (choice,obj) =>{
     if(choice === "DEFAULT"){
@@ -31,13 +38,19 @@ function AllStrains(){
     }
 
   }
+
   useEffect(()=>{
     //setStrain(strain.sort((a, b) => (a.seedCompany.name > b.seedCompany.name) ? 1 : -1))
     
     M.AutoInit();
+    const results = OG.filter((a) =>
+      a.Name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setOgstrain(results);
+    
 
   
-  },[])
+  },[searchTerm])
 
   return <div>
   <Banner classes="parallax-container">
@@ -48,7 +61,7 @@ function AllStrains(){
   
   
  <Row>
-  <div className="input-field col s4 offset-s4">
+  <div className="input-field col s4 offset-s2">
     <select defaultValue={'DEFAULT'} onChange={e=> setOgstrain(filterResults(e.currentTarget.value,OG)) }>
       <option value="DEFAULT" disabled >Choose your Strain!</option>
       <option value="All">All</option>
@@ -57,11 +70,21 @@ function AllStrains(){
       <option value="sativa">Sativa</option>
     </select>
   </div>
+  <div className="col s4">
+      <form>
+        <div class="input-field">
+        <i class="material-icons prefix">search</i>
+          <input id="search" type="search" placeholder="Search by name?" value={searchTerm} onChange={handleChange} required></input>
+          <label class="label-icon" for="search"></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+    </div>
  </Row>
 
   <Row>
-    <Container>
-      {ogStrain.slice(0,150).map((ogStrain,index)=>(
+    <Container >
+      {ogStrain.slice(0,500).map((ogStrain,index)=>(
         <Col key={index} size="s6 m4">
           <Card  classes="card hoverable medium">
           <Card  classes="card-image waves-effect waves-block waves-light">
