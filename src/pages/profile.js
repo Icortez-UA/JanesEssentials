559677ebf191624d4cbf8c25516f6246f47afecd
Israel Beltran <56612458+israelb213@users.noreply.github.com>
2020-04-12 00:45:04 -0700

    simple map and

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,12 +1,14 @@
 import React, {useState,useEffect} from 'react';
-
+import M from "materialize-css"
 import {Col, Container, Row} from "../../components/grid/index"
 import Card from "../../components/Card/card"
 import All from "../../utils/data1.json"
 import OG from "../../utils/janesSampledata.json"
-import Logo from "../../assets/imgs/icon-192x192.png"
 import Banner from "../../components/Parallax/parallax"
 import BannerImg from "../../assets/imgs/janesParalax.jpg"
+import {GoogleMap, GoogleApiWrapper} from "google-maps-react";
+import SimpleMap from "../../components/map/map"
+
 
 
   const Profile = ()=> {
@@ -17,7 +19,7 @@ import BannerImg from "../../assets/imgs/janesParalax.jpg"
     <Banner classes="parallax-container">
    <div class="section no-pad-bot">
       <Container>
-        <h1 class="header center white-text text-lighten-2">Hello Name!</h1>
+        <h1 class="header center white-text text-lighten-2">Hello!</h1>
         
       </Container>
     </div>
@@ -28,17 +30,43 @@ import BannerImg from "../../assets/imgs/janesParalax.jpg"
 
   
         <Container>
-            <Row>
-                <Col size="s6 m12">
+            <Row size="m6 s12">
+                <Col>
                     <Card classes="card">
                         <Card classes="card-title">
                             <h1>Favorites</h1>
                         </Card>
                     </Card>
                 </Col>
+                <Col>
+                <Card classes="card">
+                  <Card classes="card-title">
+                    <h1>Locations</h1>
+                  </Card>
+                </Card>
+                </Col>
+                <Col>
+                <Card classes="card">
+                  <Card classes="card-title">
+                    <h1>Map</h1>
+                    <div class="col s12 m4">
+          <SimpleMap classes= "map-container">
+            <GoogleMap class="card grey lighten-1">
+               <div class="card-content">
+                  <div id="map"></div>
+               </div>
+            </GoogleMap>
+            </SimpleMap>
+         </div>
+
+                  </Card>
+                </Card>
+                </Col>
             </Row>
         </Container>
         </div>
-}
+  }
+
+  
 
 export default Profile;
