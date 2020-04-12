8a01b0a3ecccb2af775fcf97476bb330a8d0226d
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-04-11 21:03:55 -0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    connectionString: "mongodb://<Isaac>:<Janes420>@ds017672.mlab.com:17672/heroku_9g358wqb",
+    connectionString: "mongodb://Isaac:Janes420@ds017672.mlab.com:17672/heroku_9g358wqb",
     secret: "secret string for jwt it can be any string"
 }
