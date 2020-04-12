e10bfd6bbdb1f6a4a1eedd28a30c56fb2b0903d5
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-04-11 20:45:11 -0700

    update for env

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const path = require("path");
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(cors());
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'build')));
+  }
 //use this to secure api
 
 // api routes
