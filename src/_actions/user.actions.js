ba9a562d64a47c10155e9f7fb973627447fdac9d
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-04-18 09:29:41 -0700

    update login

--- src/_actions/user.actions.js
+++ src/_actions/user.actions.js
@@ -19,7 +19,7 @@ function login(username, password) {
             .then(
                 user => { 
                     dispatch(success(user));
-                    history.push('/');
+                    history.push('/home');
                 },
                 error => {
                     dispatch(failure(error.toString()));
@@ -46,7 +46,7 @@ function register(user) {
             .then(
                 user => { 
                     dispatch(success());
-                    history.push('/login');
+                    history.push('/LoginPage');
                     dispatch(alertActions.success('Registration successful'));
                 },
                 error => {
